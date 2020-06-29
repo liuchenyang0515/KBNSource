1666fa1a271fd6188f7dbe37410876013a98ddfc
liuchenyang <897418643@qq.com>
2020-06-29 22:20:12 +0800

    缩小，测试model

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -3,7 +3,7 @@
  * https://github.com/stevenjoezhang/live2d-widget
  */
 
-const myVersion = "v0.1.8"; // 加不加v都可以访问
+const myVersion = "v0.1.9"; // 加不加v都可以访问
 function loadWidget(config) {
 	let { waifuPath, apiPath, cdnPath } = config;
 	let useCDN = false, modelList;
@@ -163,7 +163,7 @@ function loadWidget(config) {
 			modelTexturesId = localStorage.getItem("modelTexturesId");
 		if (modelId === null) {
 			// 首次访问加载 指定模型 的 指定材质
-			modelId = 1; // 模型 ID
+			modelId = 5; // 模型 ID，测试随机
 			modelTexturesId = 53; // 材质 ID
 		}
 		loadModel(modelId, modelTexturesId);
